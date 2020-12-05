cd0d1dd9ad59b22df7369771289080f13581d31d
Tom Curran <tom@tomcurran.org>
2020-12-05 14:58:58 +0000

    Day 1 part 2

--- AdventOfCode2020/AdventOfCode2020/Day1.cs
+++ AdventOfCode2020/AdventOfCode2020/Day1.cs
@@ -8,6 +8,11 @@ namespace AdventOfCode2020
     {
         static void Main(string[] args)
         {
+            PartTwo();
+        }
+  
+        static void PartOne()
+        {
             var inputs = File.ReadAllLines("day1-input.txt")
                 .Select(int.Parse)
                 .ToHashSet();
@@ -18,6 +23,25 @@ namespace AdventOfCode2020
                 {
                     Console.WriteLine(input * find);
                     break;
+                }
+            }
+        }
+
+        static void PartTwo()
+        {
+            var inputs = File.ReadAllLines("day1-input.txt")
+                .Select(int.Parse)
+                .ToHashSet();
+            foreach (var input in inputs)
+            {
+                foreach (var input2 in inputs)
+                {
+                    var find = 2020 - input - input2;
+                    if (inputs.Contains(find))
+                    {
+                        Console.WriteLine(input * input2 * find);
+                        return;
+                    }
                 }
             }
         }
