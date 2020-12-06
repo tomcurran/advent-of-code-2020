097489e38a3a74d4580b3c562eec3112516193cd
Tom Curran <tom@tomcurran.org>
2020-12-06 18:54:00 +0000

    Day 3 part 2

--- Day3.cs
+++ Day3.cs
@@ -10,6 +10,28 @@ namespace AdventOfCode2020
         {
             const int right = 3;
             const int down = 1;
+            var trees = GetTrees(right, down);
+            Console.WriteLine(trees);
+        }
+
+        public static void Part2()
+        {
+            var directions = new []
+            {
+                (right: 1, down: 1),
+                (right: 3, down: 1),
+                (right: 5, down: 1),
+                (right: 7, down: 1),
+                (right: 1, down: 2),
+            };
+            var trees = directions
+                .Select(direction => GetTrees(direction.right, direction.down))
+                .Aggregate(1L, (x, y) => x * y);
+            Console.WriteLine(trees);
+        }
+
+        private static int GetTrees(int right, int down)
+        {
             var inputs = File.ReadAllLines("day3-input.txt");
             var inputColumns = inputs.First().Length;
             var column = right;
@@ -25,7 +47,7 @@ namespace AdventOfCode2020
                 row += down;
                 column += right;
             }
-            Console.WriteLine(trees);
+            return trees;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Day3.Part1();
+            Day3.Part2();
         }
     }
 }
