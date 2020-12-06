b0ca0d82a6dd340396f78e6f485a9a1c099986c9
Tom Curran <tom@tomcurran.org>
2020-12-06 22:02:49 +0000

    Day 4 part 1

---
+++ Day4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020
+{
+    static class Day4
+    {
+        public static void Part1()
+        {
+            var fields = new[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+            var input = File.ReadAllText("day4-input.txt");
+            var passports = Regex.Split(input, @"\n\n");
+            var keysRegex = new Regex(@"(?<key>\S+):");
+            var validPassports = passports.Count(passport =>
+            {
+                var keyMatches = keysRegex.Matches(passport);
+                var keys = keyMatches.Select(x => x.Groups["key"].Value);
+                return fields.All(field => keys.Contains(field));
+            });
+            Console.WriteLine(validPassports);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Day3.Part2();
+            Day4.Part1();
         }
     }
 }
