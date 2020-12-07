bccf331a1615f9837f94e9b113334a6190de2cba
Tom Curran <tom@tomcurran.org>
2020-12-07 23:37:12 +0000

    Day 6 part 2

--- Day6.cs
+++ Day6.cs
@@ -14,5 +14,17 @@ namespace AdventOfCode2020
             var yesAnswers = answers.Sum(answer => string.Join("", answer.Replace("\n", string.Empty).Distinct()).Length);
             Console.WriteLine(yesAnswers);
         }
+
+        public static void Part2()
+        {
+            var input = File.ReadAllText("day6-input.txt");
+            var answers = Regex.Split(input, @"\n\n");
+            var yesAnswers = answers.Sum(answer => {
+                var uniqueLetters = string.Join("", answer.Replace("\n", string.Empty).Distinct());
+                var individualAnswers = answer.Split("\n").Where(x => !string.IsNullOrEmpty(x));
+                return uniqueLetters.Count(letter => individualAnswers.All(individualAnswer => individualAnswer.Contains(letter)));
+            });
+            Console.WriteLine(yesAnswers);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Day6.Part1();
+            Day6.Part2();
         }
     }
 }
