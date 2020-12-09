677750e98beb042608ab9d6fb931333e5a0d2ef0
Tom Curran <tom@tomcurran.org>
2020-12-09 10:44:20 +0000

    Tidy day 7

--- Day7.cs
+++ Day7.cs
@@ -11,40 +11,32 @@ namespace AdventOfCode2020
         public static void Part1()
         {
             var inputs = File.ReadAllLines("day7-input.txt");
-
             var bags = inputs.ToDictionary(
                 input => input.Split(" bags contain ")[0],
-                input =>
-                {
-                    var bagsRegex = new Regex(@"( (?<bag>[a-z ]+) bags?)+");
-                    var bagsMatches = bagsRegex.Matches(input.Split(" bags contain ")[1]);
-                    var bagContents = bagsMatches.Select(x => x.Groups["bag"].Value).SkipWhile(x => x == "other");
-                    return bagContents;
-                });
+                input => new Regex(@"( (?<bag>[a-z ]+) bags?)+")
+                            .Matches(input.Split(" bags contain ")[1])
+                            .Select(x => x.Groups["bag"].Value)
+                            .SkipWhile(x => x == "other"));
 
             var myBag = "shiny gold";
             var bagsContainingMyBag = bags.Count(bag => BagContainsMyBag(bags, myBag, bag.Value));
             Console.WriteLine(bagsContainingMyBag);
 
-            bool BagContainsMyBag(Dictionary<string, IEnumerable<string>> allBags, string myBag, IEnumerable<string> bagContents)
-                => bagContents.Contains(myBag) || bagContents.Any(bag => BagContainsMyBag(allBags, myBag, allBags[bag]));
+            bool BagContainsMyBag(Dictionary<string, IEnumerable<string>> bags, string myBag, IEnumerable<string> bagContents)
+                => bagContents.Contains(myBag) || bagContents.Any(bag => BagContainsMyBag(bags, myBag, bags[bag]));
         }
 
         public static void Part2()
         {
             var inputs = File.ReadAllLines("day7-input.txt");
-
             var bags = inputs.ToDictionary(
                 input => input.Split(" bags contain ")[0],
-                input =>
-                {
-                    var bagsRegex = new Regex(@"((?<quantity>\d) (?<bag>[a-z ]+) bags?)+");
-                    var bagsMatches = bagsRegex.Matches(input.Split(" bags contain ")[1]);
-                    var bagContents = bagsMatches.Select(x => (quantity: int.Parse(x.Groups["quantity"].Value), bag: x.Groups["bag"].Value));
-                    return bagContents;
-                });
+                input => new Regex(@"((?<quantity>\d) (?<bag>[a-z ]+) bags?)+")
+                            .Matches(input.Split(" bags contain ")[1])
+                            .Select(x => (quantity: int.Parse(x.Groups["quantity"].Value), bag: x.Groups["bag"].Value)));
 
-            var myBagContents = bags["shiny gold"];
+            var myBag = "shiny gold";
+            var myBagContents = bags[myBag];
             var myBagsTotal = GetBagsInside(bags, myBagContents);
             Console.WriteLine(myBagsTotal);
 
