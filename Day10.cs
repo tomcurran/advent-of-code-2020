59a74ba52793182af61530e3f1d59db89ffc7812
Tom Curran <tom@tomcurran.org>
2020-12-10 23:46:33 +0000

    Day 10 part 1

---
+++ Day10.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    static class Day10
+    {
+        public static void Part1()
+        {
+            var numbers = File.ReadAllLines("day10-input.txt")
+                .Select(input => long.Parse(input))
+                .Prepend(0)
+                .OrderBy(x => x);
+
+            var oneCount = 0;
+            var threeCount = 1;
+
+            for (var i = 0; i < numbers.Count() - 1; i++)
+            {
+                var diff = numbers.ElementAt(i + 1) - numbers.ElementAt(i);
+                if (diff == 1)
+                {
+                    oneCount++;
+                }
+                else if (diff == 3)
+                {
+                    threeCount++;
+                }
+            }
+
+            Console.WriteLine(oneCount * threeCount);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Day9.Part2();
+            Day10.Part1();
         }
     }
 }
