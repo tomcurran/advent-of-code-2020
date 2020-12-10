36db33bcb2b245e3dd9f6a7391ad7317dcff1172
Tom Curran <tom@tomcurran.org>
2020-12-10 18:55:55 +0000

    Tidy day 8

--- Day8.cs
+++ Day8.cs
@@ -10,62 +10,28 @@ namespace AdventOfCode2020
     {
         public static void Part1()
         {
-            var regex = new Regex(@"(?<operation>[a-z]+) (?<sign>[+-])(?<number>\d+)");
-            var instructions = File.ReadAllLines("day8-input.txt")
-                .Select(input =>
-                {
-                    var match = regex.Match(input);
-                    var operation = match.Groups["operation"].Value;
-                    var sign = match.Groups["sign"].Value;
-                    var number = int.Parse(match.Groups["number"].Value);
-                    return (operation, sign, number);
-                });
-
+            var instructions = GetInstructions();
             var accumulator = 0;
             var visited = new HashSet<int>();
             for (var i = 0; !visited.Contains(i) && i < instructions.Count(); i++)
             {
-                var (operation, sign, number) = instructions.ElementAt(i);
+                visited.Add(i);
+                var (operation, number) = instructions.ElementAt(i);
                 if (operation == "acc")
                 {
-                    if (sign == "+")
-                    {
-                        accumulator += number;
-                    }
-                    else if(sign == "-")
-                    {
-                        accumulator -= number;
-                    }
+                    accumulator += number;
                 }
                 else if (operation == "jmp")
                 {
-                    if (sign == "+")
-                    {
-                        i += number - 1;
-                    }
-                    else if (sign == "-")
-                    {
-                        i -= number + 1;
-                    }
+                    i += number - 1;
                 }
-                visited.Add(i);
             }
             Console.WriteLine(accumulator);
         }
 
         public static void Part2()
         {
-            var regex = new Regex(@"(?<operation>[a-z]+) (?<sign>[+-])(?<number>\d+)");
-            var instructions = File.ReadAllLines("day8-input.txt")
-                .Select(input =>
-                {
-                    var match = regex.Match(input);
-                    var operation = match.Groups["operation"].Value;
-                    var sign = match.Groups["sign"].Value;
-                    var number = int.Parse(match.Groups["number"].Value);
-                    return (operation, sign, number);
-                });
-
+            var instructions = GetInstructions();
             for (var insturctionIndex = 0; insturctionIndex < instructions.Count(); insturctionIndex++)
             {
                 if (instructions.ElementAt(insturctionIndex).operation == "acc")
@@ -81,7 +47,7 @@ namespace AdventOfCode2020
                 }
             }
 
-            static (bool terminated, int accumulator) ExecuteInstructions(IEnumerable<(string operation, string sign, int number)> instructions, int changeInstructionIndex)
+            static (bool terminated, int accumulator) ExecuteInstructions(IEnumerable<(string operation, int number)> instructions, int changeInstructionIndex)
             {
                 var accumulator = 0;
                 var visited = new HashSet<int>();
@@ -91,41 +57,34 @@ namespace AdventOfCode2020
                     {
                         return (false, accumulator);
                     }
-                    else
-                    {
-                        visited.Add(i);
-                    }
-                    var (operation, sign, number) = instructions.ElementAt(i);
+                    visited.Add(i);
+                    var (operation, number) = instructions.ElementAt(i);
                     if (operation == "acc")
                     {
-                        if (sign == "+")
-                        {
-                            accumulator += number;
-                        }
-                        else if (sign == "-")
-                        {
-                            accumulator -= number;
-                        }
+                        accumulator += number;
                     }
                     else if ((operation == "jmp" && i != changeInstructionIndex)
                         || (operation == "nop" && i == changeInstructionIndex))
                     {
-                        if (sign == "+")
-                        {
-                            i += number - 1;
-                        }
-                        else if (sign == "-")
-                        {
-                            i -= number + 1;
-                        }
-                        if (i < 0)
-                        {
-                            return (false, accumulator);
-                        }
+                        i += number - 1;
                     }
                 }
                 return (true, accumulator);
             }
+        }
+
+        private static IEnumerable<(string operation, int number)> GetInstructions()
+        {
+            var regex = new Regex(@"(?<operation>[a-z]+) (?<number>[+-]\d+)");
+            var instructions = File.ReadAllLines("day8-input.txt")
+                .Select(input =>
+                {
+                    var match = regex.Match(input);
+                    var operation = match.Groups["operation"].Value;
+                    var number = int.Parse(match.Groups["number"].Value);
+                    return (operation, number);
+                });
+            return instructions;
         }
     }
 }
